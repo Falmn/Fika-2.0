3fa63a03c1861fe764a066556e149c08f95c219b
Oliver Knorn <oliver.knorn@gmail.com>
2021-11-23 09:30:51 +0000

    added the firebasestorage code

--- app/src/main/java/ie/ul/fika_20/NewPost.java
+++ app/src/main/java/ie/ul/fika_20/NewPost.java
@@ -24,6 +24,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -36,7 +38,7 @@ public class NewPost extends AppCompatActivity {
     ImageView selectedImage;
     Button cameraBtn, galleryBtn;
     String currentPhotoPath;
-   // StorageReference storageReference;
+    // StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
