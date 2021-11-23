c3aeba5fc9791cc87b6b1f7907d5e9eb7022a33f
frejaboholm <frejaboholm@gmail.com>
2021-11-23 14:07:06 +0000

    main + post xml

--- app/src/main/java/ie/ul/fika_20/MainActivity.java
+++ app/src/main/java/ie/ul/fika_20/MainActivity.java
@@ -5,21 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import ie.ul.fika_20.Fragments.userProfile;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private ImageView navProfile;
+    private ImageView navHome;
+    private ImageView navNewPost;
     private Fragment selectorFragment;
+    DatabaseReference fRDB;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.top_toolbar);
+        navProfile = findViewById(R.id.nav_profile);
+        navHome = findViewById(R.id.nav_home);
+        navNewPost = findViewById(R.id.nav_add_post);
+        fAuth = FirebaseAuth.getInstance();
+        fRDB = FirebaseDatabase.getInstance().getReference();
 
+        navProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new userProfile()).commit();
+            }
+        });
+
+        navNewPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NewPost.class));
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = fAuth.getCurrentUser();
+        if(currentUser == null){
+            startActivity(new Intent(MainActivity.this, Login.class));
+            finish();
+        }
     }
 
 
