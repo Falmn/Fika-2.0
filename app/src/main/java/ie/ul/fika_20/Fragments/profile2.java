c811747cf5a4fbc8a84ee01ccc32b0356b0c682b
frejaboholm <frejaboholm@gmail.com>
2021-11-29 09:02:35 +0000

    Cleaned up Profile

--- app/src/main/java/ie/ul/fika_20/Fragments/profile2.java
+++ app/src/main/java/ie/ul/fika_20/Fragments/profile2.java
@@ -1,8 +1,6 @@
 
 package ie.ul.fika_20.Fragments;
 
-import static android.content.Context.MODE_PRIVATE;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,7 +15,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
